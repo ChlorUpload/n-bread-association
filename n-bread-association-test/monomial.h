90c95065a1ead0d0e0f76fb8326d4b199fd0b059
chlorUpload <chlor.upload@gmail.com>
2020-11-01 21:08:38 +0900

    복수/단수 지키도록 이름 변경

---
+++ n-bread-association-test/monomial.h
@@ -0,0 +1,10 @@
+#ifndef __H_MONOMIAL__
+#define __H_MONOMIAL__
+
+class Monomial
+{
+	Monomial() {}
+	~Monomial() {}
+};
+
+#endif
---
+++ n-bread-association-test/polynomial.h
@@ -0,0 +1,10 @@
+#ifndef __H_POLYNOMIAL__
+#define __H_POLYNOMIAL__
+
+class Polynomial
+{
+	Polynomial() {}
+	~Polynomial() {}
+};
+
+#endif
