3e29e05ec8bc5f7ae6f45cc4a447020b0e003b9d
조민호 <mino22539643@gmail.com>
2020-11-04 01:19:34 +0900

    polynomial.cpp의 다항식을 받아서 처리하는 생성자의 부분을 작성

--- n-bread-association-test/polynomial.cpp
+++ n-bread-association-test/polynomial.cpp
@@ -3,7 +3,7 @@
 
 Polynomial::Polynomial()
 {
-
+	_monomials.push_back(Monomial());
 }
 
 Polynomial::~Polynomial()
@@ -13,7 +13,17 @@ Polynomial::~Polynomial()
 
 Polynomial::Polynomial(std::string& expr)
 {
-
+	std::string temp;
+	for(int i=0; i < expr.length(); i++)
+	{
+		if (expr[i] == '+' or expr[i] == '-')
+		{
+			_monomials.push_back(Monomial(temp));
+			temp.empty();
+		}
+		temp.push_back(expr[i]);
+	}
+	_monomials.push_back(Monomial(temp));
 }
 
 int Polynomial::getValue(int x)
