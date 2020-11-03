e13ed52d689179744d3dc5ca841b9be4487c941c
chlorUpload <chlor.upload@gmail.com>
2020-11-04 00:03:20 +0900

    Polynomial 클래스 함수 뼈대 정의

---
+++ n-bread-association-test/polynomial.cpp
@@ -0,0 +1,22 @@
+#include "polynomial.h"
+#include "monomial.h"
+
+Polynomial::Polynomial()
+{
+
+}
+
+Polynomial::~Polynomial()
+{
+
+}
+
+Polynomial::Polynomial(std::string& expr)
+{
+
+}
+
+int Polynomial::getValue(int x)
+{
+	return 0;
+}
