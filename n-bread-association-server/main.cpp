3c9f48b4117f8d3be0c1d5db01d218f393edf76e
chlorUpload <chlor.upload@gmail.com>
2020-11-11 16:55:18 +0900

    변경 사항 테스트 코드 작성

--- n-bread-association-server/main.cpp
+++ n-bread-association-server/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <string>
 
 #include "action-manager.h"
 #include "count-command.h"
-#include "db-context.h"
+#include "check-token-query.h"
+#include "dependency-injection.h"
 
 int main(void)
 {
-    DbContext ctx;
-    ActionManager am{&ctx};
+    DependencyInjection di;
+    ActionManager am{di};
 
     am(CountCommand{10});
+    am(CheckTokenQuery{std::string("access token")});
 }
