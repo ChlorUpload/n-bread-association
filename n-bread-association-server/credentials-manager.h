9264073b7793f3ad77087b2c3549715ada8d9170
chlorUpload <chlor.upload@gmail.com>
2020-11-12 01:50:39 +0900

    credentials-manager에 jwt-cpp를 이용한 jwt 인증 구현

--- n-bread-association-server/credentials-manager.h
+++ n-bread-association-server/credentials-manager.h
@@ -1,7 +1,75 @@
 #ifndef __H_CREDENTIALS_MANAGER__
 #define __H_CREDENTIALS_MANAGER__
 
+#define SIGNATURE_LEN 20
+
+#include "jwt-cpp/jwt.h"
+
+#include <random>
+
 class CredentialsManager
-{};
+{
+  private:
+    std::string       _signature  = "";
+    const std::string _issuer     = "nba-auth";
+    const int         _expire_sec = 20000;
+
+  public:
+    CredentialsManager()
+    {
+        std::random_device rd;
+        std::mt19937       gen { rd() };
+        std::uniform_int_distribution<int> dis(0, 25);
+
+        for (int i = 0; i < SIGNATURE_LEN; i++)
+        {
+            char rand_char = 'A' + dis(gen);
+            _signature.push_back(rand_char);
+        }
+
+        //std::cout << "signature : " << _signature << std::endl;
+    }
+
+    std::string create_token(std::string const& user_id)
+    {
+        return jwt::create()
+            .set_issuer(_issuer)
+            .set_issued_at(std::chrono::system_clock::now())
+            .set_expires_at(std::chrono::system_clock::now()
+                            + std::chrono::seconds { _expire_sec })
+            .set_type("JWS")
+            .set_payload_claim("user_id", jwt::claim(user_id))
+            .sign(jwt::algorithm::hs256 { _signature });
+    }
+
+    bool verify_token(std::string const& token)
+    {
+        auto verifier
+            = jwt::verify()
+                  .allow_algorithm(jwt::algorithm::hs256 { _signature })
+                  .with_issuer(_issuer);
+
+        auto decoded = jwt::decode(token);
+
+        try
+        {
+            verifier.verify(decoded);
+        }
+        catch (...)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    std::string get_user_id(std::string const& token)
+    {
+        auto decoded = jwt::decode(token);
+
+        auto claim = decoded.get_payload_claim("user_id");
+
+        return claim.as_string();
+    }
+};
 
 #endif
--- n-bread-association-server/main.cpp
+++ n-bread-association-server/main.cpp
@@ -1,6 +1,7 @@
 #include "action-manager.h"
 #include "check-token-query.h"
 #include "count-command.h"
+#include "credentials-manager.h"
 #include "dependency-injection.h"
 
 #include <iostream>
@@ -13,4 +14,10 @@ int main(void)
 
     am(CountCommand { 10 });
     am(CheckTokenQuery { std::string("access token") });
+
+    auto cm = di.GetService<CredentialsManager>();
+    std::string token = cm.create_token("songdaegeon");
+    std::cout << token << std::endl;
+    std::cout << std::boolalpha << cm.verify_token(token) << std::endl;
+    std::cout << cm.get_user_id(token) << std::endl;
 }
