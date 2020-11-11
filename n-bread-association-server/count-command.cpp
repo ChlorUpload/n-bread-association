a50765b44ed87baa096a5415afc72807272c1293
chlorUpload <chlor.upload@gmail.com>
2020-11-11 16:54:29 +0900

    DependencyInjection에서 서비스를 가져오도록 수정

--- n-bread-association-server/count-command.cpp
+++ n-bread-association-server/count-command.cpp
@@ -1,6 +1,27 @@
-#include "count-command.h"
-
 #include <iostream>
+#include <memory>
+
+#include "count-command.h"
+#include "db-context.h"
+
+template <>
+struct ActionHandler<CountCommand>::Impl
+{
+    DbContext& ctx;
+
+    Impl(DbContext& ctx)
+        : ctx{ctx}
+    { }
+};
+
+template <>
+ActionHandler<CountCommand>::ActionHandler(DependencyInjection& di)
+    : pImpl{std::make_unique<Impl>(di.GetService<DbContext>())}
+{ }
+
+template <>
+ActionHandler<CountCommand>::~ActionHandler()
+{ }
 
 template <>
 void ActionHandler<CountCommand>::operator()(CountCommand const& action)
