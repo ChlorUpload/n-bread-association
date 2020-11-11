0e367d4e19031b297ca86c9f0ef21c4ce16a2163
chlorUpload <chlor.upload@gmail.com>
2020-11-11 13:49:22 +0900

    HTTP asynchronous 서버로 다운그레이드

--- n-bread-association-server-exercise/main.cpp
+++ n-bread-association-server-exercise/main.cpp
@@ -9,7 +9,7 @@
 
 //------------------------------------------------------------------------------
 //
-// Example: Advanced server
+// Example: HTTP server, asynchronous
 //
 //------------------------------------------------------------------------------
 
@@ -17,14 +17,10 @@
 
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
-#include <boost/beast/websocket.hpp>
 #include <boost/beast/version.hpp>
-#include <boost/asio/bind_executor.hpp>
 #include <boost/asio/dispatch.hpp>
-#include <boost/asio/signal_set.hpp>
 #include <boost/asio/strand.hpp>
-#include <boost/make_unique.hpp>
-#include <boost/optional.hpp>
+#include <boost/config.hpp>
 #include <algorithm>
 #include <cstdlib>
 #include <functional>
@@ -34,11 +30,10 @@
 #include <thread>
 #include <vector>
 
-namespace beast = boost::beast;                 // from <boost/beast.hpp>
-namespace http = beast::http;                   // from <boost/beast/http.hpp>
-namespace websocket = beast::websocket;         // from <boost/beast/websocket.hpp>
-namespace net = boost::asio;                    // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;               // from <boost/asio/ip/tcp.hpp>
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 // Return a reasonable mime type based on the extension of a file.
 beast::string_view
@@ -116,8 +111,6 @@ template<
         http::request<Body, http::basic_fields<Allocator>>&& req,
         Send&& send)
 {
-    std::cout << "request income" << std::endl;
-
     // Returns a bad request response
     auto const bad_request =
         [&req](beast::string_view why)
@@ -221,66 +214,92 @@ fail(beast::error_code ec, char const* what)
     std::cerr << what << ": " << ec.message() << "\n";
 }
 
-// Echoes back all received WebSocket messages
-class websocket_session : public std::enable_shared_from_this<websocket_session>
+// Handles an HTTP server connection
+class session : public std::enable_shared_from_this<session>
 {
-    websocket::stream<beast::tcp_stream> ws_;
+    // This is the C++11 equivalent of a generic lambda.
+    // The function object is used to send an HTTP message.
+    struct send_lambda
+    {
+        session& self_;
+
+        explicit
+            send_lambda(session& self)
+            : self_(self)
+        {
+        }
+
+        template<bool isRequest, class Body, class Fields>
+        void
+            operator()(http::message<isRequest, Body, Fields>&& msg) const
+        {
+            // The lifetime of the message has to extend
+            // for the duration of the async operation so
+            // we use a shared_ptr to manage it.
+            auto sp = std::make_shared<
+                http::message<isRequest, Body, Fields>>(std::move(msg));
+
+            // Store a type-erased version of the shared
+            // pointer in the class to keep it alive.
+            self_.res_ = sp;
+
+            // Write the response
+            http::async_write(
+                self_.stream_,
+                *sp,
+                beast::bind_front_handler(
+                    &session::on_write,
+                    self_.shared_from_this(),
+                    sp->need_eof()));
+        }
+    };
+
+    beast::tcp_stream stream_;
     beast::flat_buffer buffer_;
+    std::shared_ptr<std::string const> doc_root_;
+    http::request<http::string_body> req_;
+    std::shared_ptr<void> res_;
+    send_lambda lambda_;
 
 public:
-    // Take ownership of the socket
-    explicit
-        websocket_session(tcp::socket&& socket)
-        : ws_(std::move(socket))
+    // Take ownership of the stream
+    session(
+        tcp::socket&& socket,
+        std::shared_ptr<std::string const> const& doc_root)
+        : stream_(std::move(socket))
+        , doc_root_(doc_root)
+        , lambda_(*this)
     {
     }
 
-    // Start the asynchronous accept operation
-    template<class Body, class Allocator>
+    // Start the asynchronous operation
     void
-        do_accept(http::request<Body, http::basic_fields<Allocator>> req)
+        run()
     {
-        // Set suggested timeout settings for the websocket
-        ws_.set_option(
-            websocket::stream_base::timeout::suggested(
-                beast::role_type::server));
-
-        // Set a decorator to change the Server of the handshake
-        ws_.set_option(websocket::stream_base::decorator(
-            [](websocket::response_type& res)
-            {
-                res.set(http::field::server,
-                    std::string(BOOST_BEAST_VERSION_STRING) +
-                    " advanced-server");
-            }));
-
-        // Accept the websocket handshake
-        ws_.async_accept(
-            req,
+        // We need to be executing within a strand to perform async operations
+        // on the I/O objects in this session. Although not strictly necessary
+        // for single-threaded contexts, this example code is written to be
+        // thread-safe by default.
+        net::dispatch(stream_.get_executor(),
             beast::bind_front_handler(
-                &websocket_session::on_accept,
+                &session::do_read,
                 shared_from_this()));
-    }
-
-private:
-    void
-        on_accept(beast::error_code ec)
-    {
-        if (ec)
-            return fail(ec, "accept");
-
-        // Read a message
-        do_read();
     }
 
     void
         do_read()
     {
-        // Read a message into our buffer
-        ws_.async_read(
-            buffer_,
+        // Make the request empty before reading,
+        // otherwise the operation behavior is undefined.
+        req_ = {};
+
+        // Set the timeout.
+        stream_.expires_after(std::chrono::seconds(30));
+
+        // Read a request
+        http::async_read(stream_, buffer_, req_,
             beast::bind_front_handler(
-                &websocket_session::on_read,
+                &session::on_read,
                 shared_from_this()));
     }
 
@@ -291,200 +310,6 @@ private:
     {
         boost::ignore_unused(bytes_transferred);
 
-        // This indicates that the websocket_session was closed
-        if (ec == websocket::error::closed)
-            return;
-
-        if (ec)
-            fail(ec, "read");
-
-        // Echo the message
-        ws_.text(ws_.got_text());
-        ws_.async_write(
-            buffer_.data(),
-            beast::bind_front_handler(
-                &websocket_session::on_write,
-                shared_from_this()));
-    }
-
-    void
-        on_write(
-            beast::error_code ec,
-            std::size_t bytes_transferred)
-    {
-        boost::ignore_unused(bytes_transferred);
-
-        if (ec)
-            return fail(ec, "write");
-
-        // Clear the buffer
-        buffer_.consume(buffer_.size());
-
-        // Do another read
-        do_read();
-    }
-};
-
-//------------------------------------------------------------------------------
-
-// Handles an HTTP server connection
-class http_session : public std::enable_shared_from_this<http_session>
-{
-    // This queue is used for HTTP pipelining.
-    class queue
-    {
-        enum
-        {
-            // Maximum number of responses we will queue
-            limit = 8
-        };
-
-        // The type-erased, saved work item
-        struct work
-        {
-            virtual ~work() = default;
-            virtual void operator()() = 0;
-        };
-
-        http_session& self_;
-        std::vector<std::unique_ptr<work>> items_;
-
-    public:
-        explicit
-            queue(http_session& self)
-            : self_(self)
-        {
-            static_assert(limit > 0, "queue limit must be positive");
-            items_.reserve(limit);
-        }
-
-        // Returns `true` if we have reached the queue limit
-        bool
-            is_full() const
-        {
-            return items_.size() >= limit;
-        }
-
-        // Called when a message finishes sending
-        // Returns `true` if the caller should initiate a read
-        bool
-            on_write()
-        {
-            BOOST_ASSERT(!items_.empty());
-            auto const was_full = is_full();
-            items_.erase(items_.begin());
-            if (!items_.empty())
-                (*items_.front())();
-            return was_full;
-        }
-
-        // Called by the HTTP handler to send a response.
-        template<bool isRequest, class Body, class Fields>
-        void
-            operator()(http::message<isRequest, Body, Fields>&& msg)
-        {
-            // This holds a work item
-            struct work_impl : work
-            {
-                http_session& self_;
-                http::message<isRequest, Body, Fields> msg_;
-
-                work_impl(
-                    http_session& self,
-                    http::message<isRequest, Body, Fields>&& msg)
-                    : self_(self)
-                    , msg_(std::move(msg))
-                {
-                }
-
-                void
-                    operator()()
-                {
-                    http::async_write(
-                        self_.stream_,
-                        msg_,
-                        beast::bind_front_handler(
-                            &http_session::on_write,
-                            self_.shared_from_this(),
-                            msg_.need_eof()));
-                }
-            };
-
-            // Allocate and store the work
-            items_.push_back(
-                boost::make_unique<work_impl>(self_, std::move(msg)));
-
-            // If there was no previous work, start this one
-            if (items_.size() == 1)
-                (*items_.front())();
-        }
-    };
-
-    beast::tcp_stream stream_;
-    beast::flat_buffer buffer_;
-    std::shared_ptr<std::string const> doc_root_;
-    queue queue_;
-
-    // The parser is stored in an optional container so we can
-    // construct it from scratch it at the beginning of each new message.
-    boost::optional<http::request_parser<http::string_body>> parser_;
-
-public:
-    // Take ownership of the socket
-    http_session(
-        tcp::socket&& socket,
-        std::shared_ptr<std::string const> const& doc_root)
-        : stream_(std::move(socket))
-        , doc_root_(doc_root)
-        , queue_(*this)
-    {
-    }
-
-    // Start the session
-    void
-        run()
-    {
-        // We need to be executing within a strand to perform async operations
-        // on the I/O objects in this session. Although not strictly necessary
-        // for single-threaded contexts, this example code is written to be
-        // thread-safe by default.
-        net::dispatch(
-            stream_.get_executor(),
-            beast::bind_front_handler(
-                &http_session::do_read,
-                this->shared_from_this()));
-    }
-
-
-private:
-    void
-        do_read()
-    {
-        // Construct a new parser for each message
-        parser_.emplace();
-
-        // Apply a reasonable limit to the allowed size
-        // of the body in bytes to prevent abuse.
-        parser_->body_limit(10000);
-
-        // Set the timeout.
-        stream_.expires_after(std::chrono::seconds(30));
-
-        // Read a request using the parser-oriented interface
-        http::async_read(
-            stream_,
-            buffer_,
-            *parser_,
-            beast::bind_front_handler(
-                &http_session::on_read,
-                shared_from_this()));
-    }
-
-    void
-        on_read(beast::error_code ec, std::size_t bytes_transferred)
-    {
-        boost::ignore_unused(bytes_transferred);
-
         // This means they closed the connection
         if (ec == http::error::end_of_stream)
             return do_close();
@@ -492,26 +317,15 @@ private:
         if (ec)
             return fail(ec, "read");
 
-        // See if it is a WebSocket Upgrade
-        if (websocket::is_upgrade(parser_->get()))
-        {
-            // Create a websocket session, transferring ownership
-            // of both the socket and the HTTP request.
-            std::make_shared<websocket_session>(
-                stream_.release_socket())->do_accept(parser_->release());
-            return;
-        }
-
         // Send the response
-        handle_request(*doc_root_, parser_->release(), queue_);
-
-        // If we aren't at the queue limit, try to pipeline another request
-        if (!queue_.is_full())
-            do_read();
+        handle_request(*doc_root_, std::move(req_), lambda_);
     }
 
     void
-        on_write(bool close, beast::error_code ec, std::size_t bytes_transferred)
+        on_write(
+            bool close,
+            beast::error_code ec,
+            std::size_t bytes_transferred)
     {
         boost::ignore_unused(bytes_transferred);
 
@@ -525,12 +339,11 @@ private:
             return do_close();
         }
 
-        // Inform the queue that a write completed
-        if (queue_.on_write())
-        {
-            // Read another request
-            do_read();
-        }
+        // We're done with the response so delete it
+        res_ = nullptr;
+
+        // Read another request
+        do_read();
     }
 
     void
@@ -602,15 +415,7 @@ public:
     void
         run()
     {
-        // We need to be executing within a strand to perform async operations
-        // on the I/O objects in this session. Although not strictly necessary
-        // for single-threaded contexts, this example code is written to be
-        // thread-safe by default.
-        net::dispatch(
-            acceptor_.get_executor(),
-            beast::bind_front_handler(
-                &listener::do_accept,
-                this->shared_from_this()));
+        do_accept();
     }
 
 private:
@@ -634,8 +439,8 @@ private:
         }
         else
         {
-            // Create the http session and run it
-            std::make_shared<http_session>(
+            // Create the session and run it
+            std::make_shared<session>(
                 std::move(socket),
                 doc_root_)->run();
         }
@@ -653,9 +458,9 @@ int main(int argc, char* argv[])
     if (argc != 5)
     {
         std::cerr <<
-            "Usage: advanced-server <address> <port> <doc_root> <threads>\n" <<
+            "Usage: http-server-async <address> <port> <doc_root> <threads>\n" <<
             "Example:\n" <<
-            "    advanced-server 0.0.0.0 8080 . 1\n";
+            "    http-server-async 0.0.0.0 8080 . 1\n";
         return EXIT_FAILURE;
     }
     auto const address = net::ip::make_address(argv[1]);
@@ -672,17 +477,6 @@ int main(int argc, char* argv[])
         tcp::endpoint{ address, port },
         doc_root)->run();
 
-    // Capture SIGINT and SIGTERM to perform a clean shutdown
-    net::signal_set signals(ioc, SIGINT, SIGTERM);
-    signals.async_wait(
-        [&](beast::error_code const&, int)
-        {
-            // Stop the `io_context`. This will cause `run()`
-            // to return immediately, eventually destroying the
-            // `io_context` and all of the sockets in it.
-            ioc.stop();
-        });
-
     // Run the I/O service on the requested number of threads
     std::vector<std::thread> v;
     v.reserve(threads - 1);
@@ -693,12 +487,6 @@ int main(int argc, char* argv[])
                 ioc.run();
             });
     ioc.run();
-
-    // (If we get here, it means we got a SIGINT or SIGTERM)
-
-    // Block until all the threads exit
-    for (auto& t : v)
-        t.join();
 
     return EXIT_SUCCESS;
 }
