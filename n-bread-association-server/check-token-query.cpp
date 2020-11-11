890e7d8493e8b00e2ab82056dfac27b3fd0bb2ca
chlorUpload <chlor.upload@gmail.com>
2020-11-11 16:54:56 +0900

    credentials-manager 클래스 구현 및 check-token-query 추가

---
+++ n-bread-association-server/check-token-query.cpp
@@ -0,0 +1,34 @@
+#include <memory>
+
+#include "check-token-query.h"
+#include "credentials-manager.h"
+#include "db-context.h"
+
+template <>
+struct ActionHandler<CheckTokenQuery>::Impl
+{
+    CredentialsManager& cm;
+    DbContext& ctx;
+
+    Impl(CredentialsManager& cm, DbContext& ctx)
+        : cm{cm}
+        , ctx{ctx}
+    { }
+};
+
+template <>
+ActionHandler<CheckTokenQuery>::ActionHandler(DependencyInjection& di)
+    : pImpl{std::make_unique<Impl>(di.GetService<CredentialsManager>(),
+                                   di.GetService<DbContext>())}
+{ }
+
+template <>
+ActionHandler<CheckTokenQuery>::~ActionHandler()
+{ } 
+
+template <>
+bool ActionHandler<CheckTokenQuery>::operator()(CheckTokenQuery const& action)
+{
+    std::cout << action.access_token << std::endl;
+    return true;
+}
---
+++ n-bread-association-server/credentials-manager.h
@@ -0,0 +1,9 @@
+#ifndef __H_CREDENTIALS_MANAGER__
+#define __H_CREDENTIALS_MANAGER__
+
+class CredentialsManager
+{
+
+};
+
+#endif
