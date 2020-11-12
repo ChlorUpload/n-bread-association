66e6c5922bb29482ae780681f6e8adcb85035df2
chlorUpload <chlor.upload@gmail.com>
2020-11-13 04:10:45 +0900

    synchronous 서버 테스트

--- n-bread-association-server-exercise/main.cpp
+++ n-bread-association-server-exercise/main.cpp
@@ -9,7 +9,7 @@
 
 //------------------------------------------------------------------------------
 //
-// Example: HTTP server, asynchronous
+// Example: HTTP server, synchronous
 //
 //------------------------------------------------------------------------------
 
@@ -18,22 +18,20 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
-#include <boost/asio/dispatch.hpp>
-#include <boost/asio/strand.hpp>
+#include <boost/asio/ip/tcp.hpp>
 #include <boost/config.hpp>
-#include <algorithm>
 #include <cstdlib>
-#include <functional>
 #include <iostream>
 #include <memory>
 #include <string>
 #include <thread>
-#include <vector>
 
 namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
 namespace net = boost::asio;            // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+
+//------------------------------------------------------------------------------
 
 // Return a reasonable mime type based on the extension of a file.
 beast::string_view
@@ -108,8 +106,8 @@ template<
     void
     handle_request(
         beast::string_view doc_root,
-        http::request<Body, http::basic_fields<Allocator>>&& req,
-        Send&& send)
+        http::request<Body, http::basic_fields<Allocator>> && req,
+        Send && send)
 {
     // Returns a bad request response
     auto const bad_request =
@@ -214,279 +212,126 @@ fail(beast::error_code ec, char const* what)
     std::cerr << what << ": " << ec.message() << "\n";
 }
 
-// Handles an HTTP server connection
-class session : public std::enable_shared_from_this<session>
+// This is the C++11 equivalent of a generic lambda.
+// The function object is used to send an HTTP message.
+template<class Stream>
+struct send_lambda
 {
-    // This is the C++11 equivalent of a generic lambda.
-    // The function object is used to send an HTTP message.
-    struct send_lambda
-    {
-        session& self_;
+    Stream& stream_;
+    bool& close_;
+    beast::error_code& ec_;
 
-        explicit
-            send_lambda(session& self)
-            : self_(self)
-        {
-        }
-
-        template<bool isRequest, class Body, class Fields>
-        void
-            operator()(http::message<isRequest, Body, Fields>&& msg) const
-        {
-            // The lifetime of the message has to extend
-            // for the duration of the async operation so
-            // we use a shared_ptr to manage it.
-            auto sp = std::make_shared<
-                http::message<isRequest, Body, Fields>>(std::move(msg));
-
-            // Store a type-erased version of the shared
-            // pointer in the class to keep it alive.
-            self_.res_ = sp;
-
-            // Write the response
-            http::async_write(
-                self_.stream_,
-                *sp,
-                beast::bind_front_handler(
-                    &session::on_write,
-                    self_.shared_from_this(),
-                    sp->need_eof()));
-        }
-    };
-
-    beast::tcp_stream stream_;
-    beast::flat_buffer buffer_;
-    std::shared_ptr<std::string const> doc_root_;
-    http::request<http::string_body> req_;
-    std::shared_ptr<void> res_;
-    send_lambda lambda_;
-
-public:
-    // Take ownership of the stream
-    session(
-        tcp::socket&& socket,
-        std::shared_ptr<std::string const> const& doc_root)
-        : stream_(std::move(socket))
-        , doc_root_(doc_root)
-        , lambda_(*this)
+    explicit
+        send_lambda(
+            Stream& stream,
+            bool& close,
+            beast::error_code& ec)
+        : stream_(stream)
+        , close_(close)
+        , ec_(ec)
     {
     }
 
-    // Start the asynchronous operation
+    template<bool isRequest, class Body, class Fields>
     void
-        run()
+        operator()(http::message<isRequest, Body, Fields>&& msg) const
     {
-        // We need to be executing within a strand to perform async operations
-        // on the I/O objects in this session. Although not strictly necessary
-        // for single-threaded contexts, this example code is written to be
-        // thread-safe by default.
-        net::dispatch(stream_.get_executor(),
-            beast::bind_front_handler(
-                &session::do_read,
-                shared_from_this()));
+        // Determine if we should close the connection after
+        close_ = msg.need_eof();
+
+        // We need the serializer here because the serializer requires
+        // a non-const file_body, and the message oriented version of
+        // http::write only works with const messages.
+        http::serializer<isRequest, Body, Fields> sr{ msg };
+        http::write(stream_, sr, ec_);
     }
+};
 
-    void
-        do_read()
+// Handles an HTTP server connection
+void
+do_session(
+    tcp::socket & socket,
+    std::shared_ptr<std::string const> const& doc_root)
+{
+    bool close = false;
+    beast::error_code ec;
+
+    // This buffer is required to persist across reads
+    beast::flat_buffer buffer;
+
+    // This lambda is used to send messages
+    send_lambda<tcp::socket> lambda{ socket, close, ec };
+
+    for (;;)
     {
-        // Make the request empty before reading,
-        // otherwise the operation behavior is undefined.
-        req_ = {};
-
-        // Set the timeout.
-        stream_.expires_after(std::chrono::seconds(30));
-
         // Read a request
-        http::async_read(stream_, buffer_, req_,
-            beast::bind_front_handler(
-                &session::on_read,
-                shared_from_this()));
-    }
-
-    void
-        on_read(
-            beast::error_code ec,
-            std::size_t bytes_transferred)
-    {
-        boost::ignore_unused(bytes_transferred);
-
-        // This means they closed the connection
+        http::request<http::string_body> req;
+        http::read(socket, buffer, req, ec);
         if (ec == http::error::end_of_stream)
-            return do_close();
-
+            break;
         if (ec)
             return fail(ec, "read");
 
         // Send the response
-        handle_request(*doc_root_, std::move(req_), lambda_);
-    }
-
-    void
-        on_write(
-            bool close,
-            beast::error_code ec,
-            std::size_t bytes_transferred)
-    {
-        boost::ignore_unused(bytes_transferred);
-
+        handle_request(*doc_root, std::move(req), lambda);
         if (ec)
             return fail(ec, "write");
-
         if (close)
         {
             // This means we should close the connection, usually because
             // the response indicated the "Connection: close" semantic.
-            return do_close();
-        }
-
-        // We're done with the response so delete it
-        res_ = nullptr;
-
-        // Read another request
-        do_read();
-    }
-
-    void
-        do_close()
-    {
-        // Send a TCP shutdown
-        beast::error_code ec;
-        stream_.socket().shutdown(tcp::socket::shutdown_send, ec);
-
-        // At this point the connection is closed gracefully
-    }
-};
-
-//------------------------------------------------------------------------------
-
-// Accepts incoming connections and launches the sessions
-class listener : public std::enable_shared_from_this<listener>
-{
-    net::io_context& ioc_;
-    tcp::acceptor acceptor_;
-    std::shared_ptr<std::string const> doc_root_;
-
-public:
-    listener(
-        net::io_context& ioc,
-        tcp::endpoint endpoint,
-        std::shared_ptr<std::string const> const& doc_root)
-        : ioc_(ioc)
-        , acceptor_(net::make_strand(ioc))
-        , doc_root_(doc_root)
-    {
-        beast::error_code ec;
-
-        // Open the acceptor
-        acceptor_.open(endpoint.protocol(), ec);
-        if (ec)
-        {
-            fail(ec, "open");
-            return;
-        }
-
-        // Allow address reuse
-        acceptor_.set_option(net::socket_base::reuse_address(true), ec);
-        if (ec)
-        {
-            fail(ec, "set_option");
-            return;
-        }
-
-        // Bind to the server address
-        acceptor_.bind(endpoint, ec);
-        if (ec)
-        {
-            fail(ec, "bind");
-            return;
-        }
-
-        // Start listening for connections
-        acceptor_.listen(
-            net::socket_base::max_listen_connections, ec);
-        if (ec)
-        {
-            fail(ec, "listen");
-            return;
+            break;
         }
     }
 
-    // Start accepting incoming connections
-    void
-        run()
-    {
-        do_accept();
-    }
+    // Send a TCP shutdown
+    socket.shutdown(tcp::socket::shutdown_send, ec);
 
-private:
-    void
-        do_accept()
-    {
-        // The new connection gets its own strand
-        acceptor_.async_accept(
-            net::make_strand(ioc_),
-            beast::bind_front_handler(
-                &listener::on_accept,
-                shared_from_this()));
-    }
-
-    void
-        on_accept(beast::error_code ec, tcp::socket socket)
-    {
-        if (ec)
-        {
-            fail(ec, "accept");
-        }
-        else
-        {
-            // Create the session and run it
-            std::make_shared<session>(
-                std::move(socket),
-                doc_root_)->run();
-        }
-
-        // Accept another connection
-        do_accept();
-    }
-};
+    // At this point the connection is closed gracefully
+}
 
 //------------------------------------------------------------------------------
 
 int main(int argc, char* argv[])
 {
-    // Check command line arguments.
-    if (argc != 5)
+    try
     {
-        std::cerr <<
-            "Usage: http-server-async <address> <port> <doc_root> <threads>\n" <<
-            "Example:\n" <<
-            "    http-server-async 0.0.0.0 8080 . 1\n";
+        // Check command line arguments.
+        if (argc != 4)
+        {
+            std::cerr <<
+                "Usage: http-server-sync <address> <port> <doc_root>\n" <<
+                "Example:\n" <<
+                "    http-server-sync 0.0.0.0 8080 .\n";
+            return EXIT_FAILURE;
+        }
+        auto const address = net::ip::make_address(argv[1]);
+        auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
+        auto const doc_root = std::make_shared<std::string>(argv[3]);
+
+        // The io_context is required for all I/O
+        net::io_context ioc{ 1 };
+
+        // The acceptor receives incoming connections
+        tcp::acceptor acceptor{ ioc, {address, port} };
+        for (;;)
+        {
+            // This will receive the new connection
+            tcp::socket socket{ ioc };
+
+            // Block until we get a connection
+            acceptor.accept(socket);
+
+            // Launch the session, transferring ownership of the socket
+            std::thread{ std::bind(
+                &do_session,
+                std::move(socket),
+                doc_root) }.detach();
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
-    auto const address = net::ip::make_address(argv[1]);
-    auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
-    auto const doc_root = std::make_shared<std::string>(argv[3]);
-    auto const threads = std::max<int>(1, std::atoi(argv[4]));
-
-    // The io_context is required for all I/O
-    net::io_context ioc{ threads };
-
-    // Create and launch a listening port
-    std::make_shared<listener>(
-        ioc,
-        tcp::endpoint{ address, port },
-        doc_root)->run();
-
-    // Run the I/O service on the requested number of threads
-    std::vector<std::thread> v;
-    v.reserve(threads - 1);
-    for (auto i = threads - 1; i > 0; --i)
-        v.emplace_back(
-            [&ioc]
-            {
-                ioc.run();
-            });
-    ioc.run();
-
-    return EXIT_SUCCESS;
 }
