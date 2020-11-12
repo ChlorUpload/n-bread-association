d4535c3cf3d2e17cf7f12d17731931ffdbe7fa50
chlorUpload <chlor.upload@gmail.com>
2020-11-12 15:52:52 +0900

    변경 사항 테스트 코드 작성

--- n-bread-association-server/main.cpp
+++ n-bread-association-server/main.cpp
@@ -1,8 +1,9 @@
 #include "action-manager.h"
 #include "check-token-query.h"
 #include "count-command.h"
-#include "credentials-manager.h"
+#include "credentials-service.h"
 #include "dependency-injection.h"
+#include "encryption-service.h"
 
 #include <iostream>
 #include <string>
@@ -15,9 +16,12 @@ int main(void)
     am(CountCommand { 10 });
     am(CheckTokenQuery { std::string("access token") });
 
-    auto cm = di.GetService<CredentialsManager>();
-    std::string token = cm.create_token("songdaegeon");
+    auto        cm    = di.GetService<CredentialsService>();
+    std::string token = cm.create_token(156);
     std::cout << token << std::endl;
     std::cout << std::boolalpha << cm.verify_token(token) << std::endl;
     std::cout << cm.get_user_id(token) << std::endl;
+
+    auto es = di.GetService<EncryptionService>();
+    std::cout << es.encrypt("¼Û´ë°Ç") << std::endl;
 }
