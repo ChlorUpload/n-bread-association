8cfea066010bb8d56a311378dec6960897fcc906
chlorUpload <chlor.upload@gmail.com>
2020-11-12 22:58:35 +0900

    스타일 변경

--- n-bread-association-server/count-command.cpp
+++ n-bread-association-server/count-command.cpp
@@ -1,27 +1,26 @@
+#include "count-command.h"
+
+#include "db-context.h"
+
 #include <iostream>
 #include <memory>
-
-#include "count-command.h"
-#include "db-context.h"
 
 template <>
 struct ActionHandler<CountCommand>::Impl
 {
     DbContext& ctx;
 
-    Impl(DbContext& ctx)
-        : ctx{ctx}
-    { }
+    Impl(DbContext& ctx) : ctx { ctx } {}
 };
 
 template <>
-ActionHandler<CountCommand>::ActionHandler(DependencyInjection& di)
-    : pImpl{std::make_unique<Impl>(di.GetService<DbContext>())}
-{ }
+ActionHandler<CountCommand>::ActionHandler(DependencyInjection& di) :
+    pImpl { std::make_unique<Impl>(di.get_service<DbContext>()) }
+{}
 
 template <>
 ActionHandler<CountCommand>::~ActionHandler()
-{ }
+{}
 
 template <>
 void ActionHandler<CountCommand>::operator()(CountCommand const& action)
